d974ce3ac9bd50802fc427e5005e3ba38a5eef49
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-07-18 19:13:50 +0530

    Solve `33. Search in Rotated Sorted Array` in C

--- leetcode-daily/2025-07-jul/2025-07-18/2025-07-18-fun-with-sorting.c
+++ leetcode-daily/2025-07-jul/2025-07-18/2025-07-18-fun-with-sorting.c
@@ -31,3 +31,49 @@ int search(int *nums, int n, int target) {
 }
 
 /// }}
+
+
+/// {{
+/// Problem: 33. Search in Rotated Sorted Array
+/// Difficulty: `Medium`
+/// Links: https://leetcode.com/problems/search-in-rotated-sorted-array/
+/// Topics: `array`, `binary-search`
+/// Timestamp: `Fri, 18 Jul 2025 19:05:15 +0530`
+
+int binarySearch(int *arr, int target, int low, int high) {
+  for (
+    int mid = (low + high) / 2;
+    low <= high;
+    mid = (low + high) / 2
+  ) {
+    if (target < arr[mid]) {
+      high = mid - 1;
+    } else if (target > arr[mid]) {
+      low = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
+
+int search(int *nums, int n, int target) {
+  int i;
+
+  for (i = 0; (i + 1) < n; i++) {
+    if (nums[i + 1] < nums[i]) {
+      break;
+    }
+  }
+
+  int index = binarySearch(nums, target, 0, i);
+
+  if (index > -1) {
+    return index;
+  }
+
+  index = binarySearch(nums, target, i + 1, n - 1);
+  return index;
+}
+
+/// }}
