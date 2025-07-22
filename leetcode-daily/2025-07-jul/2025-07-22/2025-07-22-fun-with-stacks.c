5eb13f90690ef38f7fcb3db7491b9a3114607e50
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-07-22 15:43:39 +0530

    Attempt `71. Simplify Path` in C

--- leetcode-daily/2025-07-jul/2025-07-22/2025-07-22-fun-with-stacks.c
+++ leetcode-daily/2025-07-jul/2025-07-22/2025-07-22-fun-with-stacks.c
@@ -3,8 +3,9 @@
 // Created: `Tue, 22 Jul 2025 12:24:14 +0530`
 // Tags: [stacks, daily-practice]
 
-#include <string.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 /// {{
 /// Problem: 1598. Crawler Log Folder
@@ -35,6 +36,94 @@ int minOperations(char **logs, int logsSize) {
   }
 
   return stackSize;
+}
+
+/// }}
+
+
+/// {{
+/// Problem: 71. Simplify Path
+/// Difficulty: `Medium`
+/// Links: https://leetcode.com/problems/simplify-path/
+/// Topics: `string`, `stack`
+/// Timestamp: `Tue, 22 Jul 2025 13:08:52 +0530`
+
+#define IS_ALPHA(c) (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || (c == '_'))
+#define IS_NUMERIC(c) ('0' <= c  && c <= '9')
+#define IS_WORD_CHAR(c) (IS_ALPHA(c) || IS_NUMERIC(c) || c == '.')
+
+char *simplifyPath(char *path) {
+  const int pathLen = strlen(path);
+
+  int outputSize = 0;
+  char *output = calloc(pathLen + 1, sizeof(char));
+
+  int stack[pathLen];
+  memset(stack, 0, pathLen);
+  int stackSize = 0;
+
+  for (int i = 0; i < pathLen; i++) {
+    if (path[i] == '/') {
+      // Consume all slashes
+      for (; i + 1 < pathLen && path[i + 1] == '/'; i++) { }
+
+      if (outputSize == 0) {
+        output[outputSize++] = '/';
+      }
+    }
+    else if (path[i] == '.') {
+      int tokenStart = i, tokenLen = 1;
+      for (; i + 1 < pathLen && path[i + 1] == '.'; i++, tokenLen++) {}
+
+      if (tokenLen == 2 && (path[i + 1] == '\0' || path[i + 1] == '/')) {
+        if (stackSize > 0) {
+          int lastPos = stack[stackSize - 1];
+          stack[--stackSize] = 0;
+          memset(&output[lastPos], 0, (pathLen - lastPos) * sizeof(char));
+          outputSize = lastPos;
+        }
+      }
+      else if (tokenLen == 1 && (path[i + 1] == '\0' || path[i + 1] == '/')) {
+        // No op
+      }
+      else {
+        stack[stackSize++] = outputSize;
+
+        if (outputSize > 1) {
+          output[outputSize++] = '/';
+        }
+
+        // Copy
+        for (
+          int j = tokenStart;
+          j < tokenLen + 1;
+          (output[outputSize++] = path[j]), j++
+        ) {}
+
+        for (
+          ;
+          i + 1 < pathLen && (IS_WORD_CHAR(path[i + 1]));
+          (output[outputSize++] = path[i + 1]), i++
+        ) { }
+      }
+    }
+    else if (IS_ALPHA(path[i]) || IS_NUMERIC(path[i])) {
+      stack[stackSize++] = outputSize;
+
+      if (outputSize > 1) {
+        output[outputSize++] = '/';
+      }
+      output[outputSize++] = path[i];
+
+      for (
+        ;
+        i + 1 < pathLen && (IS_WORD_CHAR(path[i + 1]));
+        (output[outputSize++] = path[i + 1]), i++
+      ) { }
+    }
+  }
+
+  return output;
 }
 
 /// }}
